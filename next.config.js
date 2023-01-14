d0d9cdd72b2c9591e121b3fd15ee5e195dc8f564
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2023-01-14 10:10:39 +0100

    minor problem

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = {
-  async rewrites() {
+  rewrites: async () => {
     return [
-      { 
+      {
         source: '/api/:path*',
-        destination: (process.env.API_HOST||"http://127.0.0.1:8000") + '/:path*'
-      },
+        destination: (process.env.API_HOST || "http://127.0.0.1:8000") + '/:path*'
+      }
     ]
   }
 }
+
+module.exports = nextConfig
